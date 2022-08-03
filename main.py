8d76f3d36a6019ea5688957c80bcc9d00bac1375
Chris Barker <github@mrz.angrydome.com>
2022-08-03 18:24:00 +0100

    Working version TBD: make it work as a container configure a pod security role for it? magic?

--- main.py
+++ main.py
@@ -1,26 +1,81 @@
 import boto3
+import base64
+import json
+import os
+
 from kubernetes import client, config
 from pprint import pprint
 
-ecr_client = boto3.client('ecr')
+"""
+apiVersion: v1
+kind: Secret
+metadata:
+  name: myregistrykey
+  namespace: awesomeapps
+data:
+  .dockerconfigjson: UmVhbGx5IHJlYWxseSByZWVlZWVlZWVlZWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGx5eXl5eXl5eXl5eXl5eXl5eXl5eSBsbGxsbGxsbGxsbGxsbG9vb29vb29vb29vb29vb29vb29vb29vb29vb25ubm5ubm5ubm5ubm5ubm5ubm5ubm5ubmdnZ2dnZ2dnZ2dnZ2dnZ2dnZ2cgYXV0aCBrZXlzCg==
+type: kubernetes.io/dockerconfigjson
+
+"""
+
+
+def extract_config(data: str) -> dict:
+    config_dict = json.loads(decode64(data['.dockerconfigjson']))
+    return config_dict
+
+def decode64(encoded: str) -> str:
+    decoded_bytes = base64.b64decode(encoded)
+    decoded = decoded_bytes.decode('utf-8')
+    return decoded
+
+def encode64(decoded: str) -> str:
+    decoded_bytes = decoded.encode('utf-8')
+    encoded = base64.b64encode(decoded_bytes)
+    return encoded.decode('utf-8')
+
+def get_env_vars() -> dict:
+    vars = {}
+    vars['server'] = os.environ['ECR_SERVER']
+    vars['secret-name'] = os.environ['GITPOD_REG_SECRET_NAME']
+    return vars
+
+def get_ecr_token() -> str:
+    ecr_client = boto3.client('ecr')
+    auth_data = ecr_client.get_authorization_token()
+    token = auth_data['authorizationData'][0]['authorizationToken']
+    return token
 
 def main():
     config.load_config()
 
+    settings = get_env_vars()
+
+    server = settings['server']
+    secret = settings['secret-name']
+
     v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
     
-    api_response = v1.read_namespaced_secret('container-registry', 'gitpod')
-    pprint(api_response)
+    api_response = v1.read_namespaced_secret(secret, 'gitpod')
+    
+    data = extract_config(api_response.data)
+
+    if server in data['auths'].keys():
+        print(f'{server} has a secret, lets update it')
+        data['auths'][server]['password'] = get_ecr_token()
+        new_auth = encode64(f"aws:{data['auths'][server]['password']}")
+        data['auths'][server]['auth'] = new_auth
 
 
-    response = ecr_client.get_authorization_token()
+    patch = {
+        'data' : {
+            '.dockerconfigjson':
+                encode64(json.dumps(data))
+        }
+    }
 
-    pprint(response)
+    pprint(patch)
+
+    patch_secret = v1.patch_namespaced_secret(secret,'gitpod', patch)
 
 
 if __name__ == '__main__':
